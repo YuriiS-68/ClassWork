9e97f9a39262d9e862d7b909bdfde8ddb9fc976a
YuriiS-68 <yurii_s@bigmir.net>
2017-05-22 19:08:57 +0300

    The sample file was changed,ExampleFive.

--- src/ExampleFive.java
+++ src/ExampleFive.java
@@ -5,16 +5,16 @@ public class ExampleFive {
     public static void main(String[] args) {
         int size = 3;
         int [][] matrix = {{1, 2, 3},
-                           {4, 12, 6},
+                           {4, 5, 6},
                            {7, 8, 9}};
-        int indexLr = 1;
-        int indexRl = size - 1;
+        //int indexLr = 1;
+        //int indexRl = size - 1;
         int resultLr = matrix[0][0];
         int resultRl = matrix[0][size - 1];
 
-        for (int i = 1; i < matrix.length; i++, indexLr++, indexRl--, size++) {
-            resultLr *= matrix[i][indexLr];
-            resultRl *= matrix[indexRl][size - 3];
+        for (int i = 1; i < matrix.length; i++) {
+            resultLr *= matrix[i][i];
+            resultRl *= matrix[i][size - i - 1];
         }
         System.out.println(resultLr);
         System.out.println(resultRl);
